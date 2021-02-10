51c0f967e986a06b5622155240b288367f4b9983
marianazubi <78882871+marianazubi@users.noreply.github.com>
2021-02-10 20:06:44 -0300

    Update Principal.java
    Alteração no último comentário.

--- MeuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
+++ MeuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -2,9 +2,9 @@
  * Meu Primeiro Programa Java
  * 
  * Este programa mostra ao aluno:
- * 1) Que um programa Java È construÌdo dentro de um projeto Java.
- * 2) Todo programa Java deve ter uma classe com mÈtodo  main.
- * 3) Como exibir informaÁıes no terminal (console = ·rea da tela de comunicaÁ„o com o usu·rio) do Jabva.
+ * 1) Que um programa Java √© constru√≠do dentro de um projeto Java.
+ * 2) Todo programa Java deve ter uma classe com m√©todo  main.
+ * 3) Como exibir informa√ß√µes no terminal (console = √°rea da tela de comunica√ß√£o com o usu√°rio) do Jabva.
  * 4) Como ler uma string do terminal (console) do Java.
  * 
  */
@@ -16,18 +16,18 @@ import java.util.Scanner;
  * @author Mariana Zubi
  * data: 03/02/2021
  */
-public class Principal { // Classe que contÈm o mÈtodo principal.
-	public static void main(String[] args) { // Este È o mÈtodo principal
+public class Principal { // Classe que cont√©m o m√©todo principal.
+	public static void main(String[] args) { // Este √© o m√©todo principal
 		
-		// SaldaÁ„o e pergunta o nome.
-		System.out.println("Ol· amigo!\nQual È o seu nome?");
+		// Salda√ß√£o e pergunta o nome.
+		System.out.println("Ol√° amigo!\nQual √© o seu nome?");
 		
 		// Ler o nome.
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
 		
-		// SaldaÁ„o especÌfica.
-		System.out.printf("Ol· %s!", nome);
+		// Salda√ß√£o espec√≠fica para o nome lido.
+		System.out.printf("Ol√° %s!", nome);
 		in.close();
 
 		
